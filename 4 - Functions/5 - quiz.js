b2f6fc38fd1fa3c561552402be005d9daa8885d9
Agif <ghifarimohammadrahadian@gmail.com>
2023-12-05 23:01:31 +0700

    feat: add quiz for functions

---
+++ 4 - Functions/5 - quiz.js
@@ -0,0 +1,84 @@
+/*
+1. Write a JavaScript function that calculates the area of a triangle. The function should take two parameters: the base and the height of the triangle. The formula for calculating the area of a triangle is (base * height) / 2.
+
+Expected Output:
+console.log(calculateTriangleArea(5, 8)); // Output: 20
+console.log(calculateTriangleArea(10, 12)); // Output: 60
+
+Requirement:
+- Use function declaration
+*/
+
+// code here
+
+
+
+
+
+/*
+2. Write a function called isEven that takes a number as a parameter and returns true if the number is even and false otherwise.
+
+Expected Output:
+console.log(isEven(4)); // Output: true
+console.log(isEven(7)); // Output: false
+
+Requirement:
+- Use function expression
+*/
+
+// code here
+
+
+
+
+
+/*
+3. Create a function called countVowels (a, i, u, e, o) that takes a string as a parameter and returns the number of vowels in the string.
+
+Expected Output:
+console.log(countVowels("programming")); // Output: 3
+console.log(countVowels("javascript")); // Output: 3
+
+Requirement:
+- Use arrow function
+*/
+
+// code here
+
+
+
+
+
+/*
+4. Write a function called capitalizeWords that takes a sentence as a parameter and returns the sentence with the first letter of each word capitalized.
+
+Expected Output:
+console.log(capitalizeWords("javascript is awesome")); // Output: "Javascript Is Awesome"
+console.log(capitalizeWords("hello world")); // Output: "Hello World"
+
+Requirement:
+- Use any function type
+*/
+
+// code here
+
+
+
+
+
+/*
+5. Write a function called reverseString that takes a string as a parameter and returns the reversed version of the string.
+
+Expected Output:
+console.log(reverseString("hello")); // Output: "olleh"
+console.log(reverseString("javascript")); // Output: "tpircsavaj"
+
+Requirement:
+- Use any function type
+*/
+
+// code here
+
+
+
+
